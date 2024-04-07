1e8cd6a96e9f53d01f775537247b8bceda6336d1
Xavier <xavier.e.santamaria.ramirez@gmail.com>
2024-04-06 23:17:13 -0500

    Prueba de clases tailwind parte 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/index.html"],
+  content: [
+    "./src/index.html",
+    "./src/app/auth/pages/register-page/register-page.component.html",
+    "./src/app/reactive/pages/basic-page/basic-page.component.html",
+    "./src/app/reactive/pages/dymanic-page/dynamic-page.component.html",
+    "./src/app/reactive/pages/switches-page/switches-page.component.html",
+  ],
   theme: {
     extend: {},
   },
